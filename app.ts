552623c7513f79e1418bc09837295086144f5d6c
Damilare O <132923712+Kahuna04@users.noreply.github.com>
2023-09-08 11:22:15 +0100

    app.ts

--- app.ts
+++ app.ts
@@ -8,35 +8,33 @@ app.get('/api', (req, res) => {
   const slackName = req.query.slack_name as string;
   const track = req.query.track as string;
 
-  // Create GET request
-  app.get("/", (req, res) => {
-  res.send("Express on Vercel")
-  });
+ // Create GET request
+    app.get("/", (req, res) => {
+        res.send("Express on Vercel");
+    });
+    // Initialize server
+    app.listen(5000, () => {
+        console.log("Running on port 5000.");
+    });
+    // Get the current day of the week
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = daysOfWeek[new Date().getDay()];
+    // Get current UTC time with validation of +/-2 hours
+    const now = new Date();
+    const utcTime = new Date(now.getTime()).toISOString().split(".")[0] + "Z";
+    // Define GitHub URLs as variables
+    const githubFileUrl = 'https://github.com/kahuna04/kanaApp/blob/master/app.js';
+    const githubRepoUrl = 'https://github.com/kahuna04/KanaApp';
 
-  // Initialize server
-  app.listen(5000, () => {
-  console.log("Running on port 5000.");
-   });
-
-  // Get the current day of the week
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const currentDay = daysOfWeek[new Date().getDay()];
-
-  // Get current UTC time with validation of +/-2 hours
-  const now = new Date();
-  const utcTime = new Date(now.getTime() + (now.getTimezoneOffset() + 120) * 60000).toISOString();
-
-  // Construct the JSON response
-  const jsonResponse = {
-    slack_name: 'kanapp',
-    current_day: 'Friday',
-    utc_time: utcTime,
-    track: 'Backend',
-    github_file_url: 'https://github.com/Kahuna04/KanaApp/edit/master/app.ts',
-    github_repo_url: 'https://github.com/kahuna04/kanapp',
-    status_code: 200,
-  };
-
+    // Construct the JSON response
+    const jsonResponse = {
+        slack_name: 'kahuna',
+        utc_time: utcTime,
+        current_day: currentDay,
+        track: 'backend',
+        github_file_url: githubFileUrl,
+        github_repo_url: githubRepoUrl,
+        status_code: 200,
   // Send the JSON response
   res.json(jsonResponse);
 });
